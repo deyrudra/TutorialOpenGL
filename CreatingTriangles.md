7138fdf57e4c4ee21df1e4c48b2b216a4992bc55
Rudra Dey <deyrudra27@gmail.com>
2025-05-25 15:58:46 -0400

    Created a Triangle and learned about the OpenGL Graphics Pipeline.

---
+++ CreatingTriangles.md
@@ -0,0 +1,38 @@
+﻿# Creating Triangles
+
+In OpenGL everything is in 3D space, however what you see on the screen is 2D. Which means, OpenGL is all about transforming 3D Coordinates to 2D Pixels that fit on the screen.
+This process is called the graphics pipeline of OpenGL.
+There are two parts to this pipeline:
+1. The first part transforms your 3D Coordinates into 2D Coordinates
+2. The second part transforms your 2D Coordinates into actual coloured pixels.
+
+**The Graphics Pipeline**
+- Takes as input a set of 3D coordinates and outputs coloured 2D pixels on the screen.
+- Can be split into several steps (where each step has one specific function) and all the steps lead into one another.
+	- These steps can happen in parallel (and thus graphics cards todays can quickly process this data). Using the GPU's many cores to run small programs for each step of the pipeline.
+	- These small programs are called **shaders.**
+	- **Shaders** are written in the OpenGL Shading Language (GLSL).
+
+**Forming a Triangle**
+- As input to the graphics pipeline we pass in a list of three 3D coordinates that form a triangle. We pass it through an array called `Vertex Data;`. This vertex data is a collection of vertices.
+	- A **vertex** is a collection of data per 3D coordinate. The data can be represented using vertex attributes (so basically any data we would like, but most likely just a color value and a 3D position).
+- How does OpenGL know to form a Triangle out of the given vertices?
+	- When drawing using the collection of coordinates/vertices, we give a hint to OpenGL to signify whether our vertices should draw out a collection of points, collection of triangles, or just one line.
+	These hints are called **primatives** and are given to OpenGL when drawing. Some of the primatives are: `GL_POINTS, GL_TRIANGLES, GL_LINE_STRIP`
+
+![PipelineSteps](https://learnopengl.com/img/getting-started/pipeline.png)
+- **Vertex Shader:** Takes as input a single vertex; with the purpose of transforming the 3D coordinates into different 3D coordinates.
+The vertex shader gives you control over how each vertex is positioned and what data it carries forward. It's essential for transforming
+3D models into the correct space for rendering and for enabling custom visual effects. The output of the vertex shader stage is optionally
+passed to the **geometry shader stage**.
+- **Geometry Shader:** Takes as input a collection of vertices that form a primitive and has the ability to generate other shpaes by making/emitting new vertices.
+As an example, a second triangle could be made out of a given shape (this shape being a collection of vertices that form a primitive).
+- **Primative/Shape Assembly:** Takes as input all the vertices (either from the Vertex Shader stage or the Geometry Shader stage) and forms one or more primitives and assembles all 
+the poitns in the primitive shape given. Example: two triangles
+- **Rasterization:** Takes as input the output of the primative assembly stage where it maps the resulting primitives to the corresponding pixels on the screen. Resulting in fragments
+for the fragment shader to use. Before this occurs though, clipping is performed; clipping discards all fragments that are outside the view (increasing performance)
+- **Fragment Shader:** Calculates the final colour of a pixel, including shading and other effects.
+- **Alpha Tests and Blending Stage:** This stage checks for corresponding depth (and stencil) value of the fragment to check if the fragment is in front or behind other objects and if it should be discarded.
+- 
+
+
--- Main.cpp
+++ Main.cpp
@@ -7,6 +7,28 @@
 // GLFW is a Graphics Library Framework used with OpenGL, OpenGL ES, and Vulkan. It allows developers to create and manage OpenGL
 // contexts, and handle all sorts of input including: joystick, keyboard, mouse, controller, etc.
 
+
+// Vertex Shader source code
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+//Fragment Shader source code
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
+"}\n\0";
+
+
+
+
+
+
 int main() {
 	
 	// Initialize GLFW
@@ -17,6 +39,17 @@ int main() {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	//Telling GLFW that we are going to use the CORE profile, which allows us to use the modern features of OPENGL.
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Vertices for Triangle
+	GLfloat vertices[] =
+	{
+		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower left corner
+		0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower right corner
+		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f // Upper corner
+	};
+
+
+
 
 	// This is the window object for GLFW, parameters are: width, height, name of window, fullscreen boolean, last one not important
 	GLFWwindow* window = glfwCreateWindow(800, 800, "TutorialOpenGL", NULL, NULL);
@@ -37,6 +70,81 @@ int main() {
 
 	// Tell OpenGL the viewport/rendering area of our window
 	glViewport(0, 0, 800, 800);
+
+
+	// Vertex Shader Stage
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL); // Inputing Vertex Shader Source Code
+	glCompileShader(vertexShader); // Need to compile shaders now, since the computer won't understand the source code.
+
+	// Fragment Shader Stage
+	GLuint fragmentShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL); // Inputing Vertex Shader Source Code
+	glCompileShader(fragmentShader); // Need to compile shaders now, since the computer won't understand the source code.
+
+	// Basically tells the program to use these shaders.
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	// Deleting the shaders because they are already in the program.
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	
+	
+	// We want to send data between the CPU and the GPU, but since it is kind've slow, we send it in big batches called Buffers.
+	// Binding in OpenGL is all about making an object, the current object.
+	// A VBO stands for Vertex Buffer Object. It a memory buffer stored in the GPU that holds vertex data. It's an array with large amounts
+	// of data, in this case we only have one set of data (our triangle vertices). 
+	// Using glBufferData we can store our data. In there the last parameter is configurable for performance.
+	// The config 1 could be, STREAM: vertices are modified once, but used few times; STATIC: vertices are modified once, and used many times
+	// ; DYNAMIC: vertices are modified many times, and used many times.
+	// The config 2 could be DRAW; READ; COPY.
+	GLuint VAO, VBO;
+
+	// Generate a Vertex Array Object (VAO)
+	glGenVertexArrays(1, &VAO);
+
+	// Generate a Vertex Buffer Object (VBO)
+	glGenBuffers(1, &VBO);
+
+	// Bind the VAO (stores all vertex attribute configs)
+	glBindVertexArray(VAO);
+
+	// Bind the VBO to GL_ARRAY_BUFFER
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	// Upload vertex data to GPU
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// Define vertex attribute layout:
+	//    - location 0 (matches layout in vertex shader)
+	//    - 3 components (x, y, z)
+	//    - data type: float
+	//    - not normalized
+	//    - stride between vertices: 3 * float
+	//    - offset: start at beginning
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+
+	// Enable the vertex attribute at location 0
+	glEnableVertexAttribArray(0);
+
+	// Unbind the VBO (good practice)
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	// Unbind the VAO (good practice)
+	glBindVertexArray(0);
+
+
+
+
+
+
+
+
+
 
 	// We will be using the graphics concept of Double Buffering using Front Buffers and Back Buffers
 
@@ -60,11 +168,28 @@ int main() {
 
 
 	// To stop the window from closing, and have the program running indefinetely, we need a while loop.
-	while (!glfwWindowShouldClose(window)) {
+	while (!glfwWindowShouldClose(window))
+	{
+		glClearColor(0.53f, 0.81f, 0.92f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3); // 0 represents the starting index of the vertices, and 3 is the # of vertices
+
+		// Need to swap buffers to update the image.
+		glfwSwapBuffers(window);
+		
+		
 		// This function actively polls the events happening to the window; this includes window movement, mouse movements, and sort of
 		// input, etc. Without this, the window will be in state of "not responding".
 		glfwPollEvents(); 
+
 	}
+
+	// Deleting all objects we've made
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteProgram(shaderProgram);
 
 
 	// This function destoys the window.
